ceb13828f12635d179fb7d6409a432ec1e7fb722
hellyaxs <eliasvitor.dev@gmail.com>
2024-11-04 18:06:55 -0300

    adicionando botao para scroll up

--- src/app/page.js
+++ src/app/page.js
@@ -7,8 +7,38 @@ import Contact from "./components/contact";
 import Footer from "./components/footer";
 import Qualification from "./components/qualification";
 import Home from "./components/home";
+import { useEffect, useState } from 'react';
+import { IoIosArrowRoundUp } from 'react-icons/io';
 
 export default function Page() {
+
+  const isBrowser = () => typeof window !== 'undefined'; //The approach recommended by Next.js
+
+  function scrollToTop() {
+      if (!isBrowser()) return;
+      window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
+
+  const [isVisible, setIsVisible] = useState(false);
+
+  const handleScroll = () => {
+    // Show the button when the user scrolls down
+    if (window.scrollY > 100) {
+      setIsVisible(true);
+    } else {
+      setIsVisible(false);
+    }
+  };
+
+  useEffect(() => {
+    // Add scroll event listener when the component mounts
+    window.addEventListener('scroll', handleScroll);
+
+    // Remove the event listener when the component unmounts
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []); 
   
   return (
     <>
@@ -22,6 +52,16 @@ export default function Page() {
         <Contact /> 
       </main>
       <Footer />
+      {isVisible &&
+      (
+      <button
+        className={`fixed bottom-0 right-0 bg-amber-500 rounded-lg  mr-8 mb-[71px] z-50 items-center text-xl flex gap-2`}
+        onClick={scrollToTop}
+      >
+        <IoIosArrowRoundUp className="inline-block h-12 w-10" />
+      </button>
+      )}
+    
   </>
   );
 
