201b91a875c9c2780cf401fd84d5709715f78da5
hellyaxs <eliasvitor.dev@gmail.com>
2024-11-06 19:40:50 -0300

    link para os repositorios

--- src/app/components/projetos.js
+++ src/app/components/projetos.js
@@ -3,13 +3,14 @@ import Image from 'next/image'
 import Title  from './utils/title';
 import {imagens} from './utils/icons-LP';
 import AllProjects from './utils/allprojects';
+import Link from 'next/link';
 export default function Projects() {
     const projects = [
-        { id: 1, title: 'Ong Cat', description: 'Adoe para um Ong', image: '/assets/images/projects/ong-cats.png',icons: [imagens.nextjs, imagens.docker] },
-        { id: 2, title: 'Computer vision', description: 'Mão 3d com mediapipe', image: '/assets/images/projects/hand3d.jpeg', icons: [imagens.python, imagens.mediapipe] },
-        { id: 3, title: 'API Rest - Nestjs', description: 'Informações sobre filmes e séries', image: '/assets/images/projects/nestjs-projetc.png', icons: [imagens.nestjs, imagens.typescript] },
-        { id: 4, title: 'Manjaro Setup', description: 'Scrpit pos-instalação para Linux', image: '/assets/images/projects/manjaro-setup.png', icons: [imagens.linux, imagens.git] },
-        { id: 5, title: 'API Rest - Spring boot', description: 'Sistema de Transções Bancarias', image: '/assets/images/projects/arquiterure.png',icons: [imagens.spring, imagens.java] },
+        { id: 1, title: 'Ong Cat', link:"https://github.com/hellyaxs/ong-cats", description: 'Adoe para um Ong', image: '/assets/images/projects/ong-cats.png',icons: [imagens.nextjs, imagens.docker] },
+        { id: 2, title: 'Computer vision',link:"https://github.com/hellyaxs/projeto-mao-3d-mediapipe", description: 'Mão 3d com mediapipe', image: '/assets/images/projects/hand3d.jpeg', icons: [imagens.python, imagens.mediapipe] },
+        { id: 3, title: 'API Rest - Nestjs',link:"https://github.com/hellyaxs/selecao-node-adasi-2024", description: 'Informações sobre filmes e séries', image: '/assets/images/projects/nestjs-projetc.png', icons: [imagens.typescript,imagens.nestjs] },
+        { id: 4, title: 'Manjaro Setup',link:"https://github.com/hellyaxs/manjaro-setup", description: 'Scrpit pos-instalação para Linux', image: '/assets/images/projects/manjaro-setup.png', icons: [imagens.linux, imagens.golang] },
+        { id: 5, title: 'API Rest - Spring boot',link:"https://github.com/hellyaxs/SantanderBankline_Api", description: 'Sistema de Transções Bancarias', image: '/assets/images/projects/arquiterure.png',icons: [imagens.spring, imagens.java] },
       ];
     
       return (
@@ -18,6 +19,7 @@ export default function Projects() {
           
           <div className="grid grid-cols-1 mx-auto sm:grid-cols-2 lg:grid-cols-3 gap-6 max-w-[1068px]">
             {projects.map((project) => (
+             <Link key={project.id} href={project.link} target='_blank'>
               <div key={project.id} className="w-full rounded-lg shadow-lg overflow-hidden transform transition duration-500 hover:scale-105">
                 <Image src={project.image} width={500} height={600} alt={project.title} className="h-48 object-cover"  />
                 <div className="absolute -left-2 inset-0 w-full bg-black bg-opacity-50 flex items-end justify-start p-4 opacity-0 transition-opacity duration-500 hover:opacity-100">
@@ -36,12 +38,14 @@ export default function Projects() {
                               height={45}
                               alt="icon"
                             />
+                            
                           ) : null
                         ))}
                     </div>
                     </div>
                 </div>
               </div>
+              </Link>
             ))}
           </div>
            <div className="hidden sm:flex justify-center xl:flex"><AllProjects  title="Todos os Projetos" /> </div>
--- src/app/components/utils/allprojects.js
+++ src/app/components/utils/allprojects.js
@@ -9,7 +9,7 @@ export default function AllProjects({ title }) {
       <div className="mt-24 flex items-center w-screen max-w-[600px] h-auto rounded-[15px] bg-dark border dark:border-blue-900">
         <div className="flex flex-col py-8 justify-start mr-10 items-center">
         <h4 className="font-black text-[1.8rem] mb-5 dark:text-emerald-600">{title}</h4>
-          <Link href={'/projects'}>
+          <Link href={'https://github.com/hellyaxs?tab=repositories'} target='_blank'>
             <button className="inline-flex justify-center dark:text-emerald-600 items-center text-center gap-[7px] cursor-pointer border-[0.25em] border-secondColor px-6 py-4 text-[1rem] font-bold bg-transparent rounded-[1em] outline-none relative -top-1 transition-all duration-300 hover:text-black hover:bg-white hover:shadow-[0_0_1em_0.25em_var(--hoverSecond),_0_0_4em_2em_var(--hoverSecond),_inset_0_0_0.75em_0.25em_white]">
               Projetos <AiOutlineAppstore size={20} />
             </button>
